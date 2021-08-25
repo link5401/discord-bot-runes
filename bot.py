d84a870c37327142ed0d786c3ba70bbb382a9a29
link5401 <sieunganinh880@gmail.com>
2021-08-25 20:21:30 +0700

    INITIAL COMIT

--- bot.py
+++ bot.py
@@ -1,141 +1,139 @@
 import discord
-import gunicorn
 import os
+import random
 import requests
 import json
 from replit import db
+from keep_alive import keep_alive
 from discord.ext import commands
-import time
-from multiprocessing.pool import ThreadPool
-import urllib.request
-import re
-import io
-from PIL import Image
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import ffmpeg
+from bs4 import BeautifulSoup
+from datetime import datetime
 client = commands.Bot(command_prefix='$')
+
 @client.event
 async def  on_ready():
     print('I am a stupid Python bot aka {0.user}'.format(client))
-
-
-#help
-client.remove_command('help')
-@client.group(invoke_without_command=True)
-async def help(ctx):
-    em = discord.Embed(title= "Help", description="Use $help <command> for instructions")
-    em.add_field(name='commands:',value='$runes\n$items\n$levels\n$runes,$items and $levels commands are only availale when im online because server nhu cac')
-    await ctx.send(embed=em)
-@help.command()
-async def runes(ctx):
-    em = discord.Embed(titled = "Runes", description= "Gets yo runes")
-    em.add_field(name = "**Syntax**", value = "$runes <champ> <lane>")
-    await ctx.send(embed = em)
-@help.command()
-async def items(ctx):
-    em = discord.Embed(titled = "Items", description= "Gets yo items")
-    em.add_field(name = "**Syntax**", value = "$items <champ> <lane>")
-    await ctx.send(embed = em)
-@help.command()
-async def levels(ctx):
-    em = discord.Embed(titled = "Levels", description= "Gets yo levels")
-    em.add_field(name = "**Syntax**", value = "$levels <champ> <lane>")
-    await ctx.send(embed = em)
-
+@client.command()
+async def bot_master(ctx):
+    github_icon = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png'
+    img_url = 'https://scontent.fsgn2-3.fna.fbcdn.net/v/t1.6435-9/134079334_1564269020627947_7628475970815779425_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=liBumNmxNLwAX9m5gfr&tn=DYlMl_xALjPkESeE&_nc_ht=scontent.fsgn2-3.fna&oh=e09d72c99c01a16af0a95bcb4d265bc7&oe=614B5C4A'
+    facebok_icon = 'https://image.similarpng.com/very-thumbnail/2020/04/Popular-Logo-facebook-icon-png.png'
+    emb = discord.Embed(title='La Duong Xuan Linh', description= 'Junior at HCMUT',color = discord.Colour.blue())
+    emb.set_thumbnail(url=img_url)
+    emb.set_author(name='Author info: ',icon_url=img_url)
+    emb.add_field(name= 'My git hub:', value= 'https://github.com/link5401', inline=True)
+    emb.add_field(name= 'My facebook: ', value= 'https://www.facebook.com/profile.php?id=100011345917376', inline=True)
+    await ctx.send(embed=emb)
 
 @client.command()
-async def runes(ctx, champ, lane):
-    async with ctx.typing():
-        print('ran into cmd')
-        google_chrome_options = webdriver.ChromeOptions()
-        google_chrome_options.add_argument('--disable-gpu')
-        google_chrome_options.add_argument('--no-sandbox')
-        google_chrome_options.add_argument('--disable-dev-shm-usage')
-        google_chrome_options.add_argument('--enable-features=NetworkService,NetworkServiceInProcess')
-        google_chrome_options.binary_location= os.environ.get('GOOGLE_CHROME_BIN')
-        google_chrome_options.headless = True
-        google_chrome_options.add_argument('--window-size=1920,3000')
-        web_driver = webdriver.Chrome(
-            executable_path=os.environ.get('CHROMEDRIVER_PATH'),
-            options=google_chrome_options
-        )
-        url = f"https://op.gg/champion/{champ}/statistics/{lane}"
-        await ctx.send("Build :\n")
-    async with ctx.typing():
-        web_driver.get(url)
-        web_driver.save_screenshot("element.png")
-        im = Image.open('element.png')
-        im = im.crop((420, 1860, 1150, 2460))
-        web_driver.close()
-        im.save('runes1.png')
-        await ctx.send(
-            file=discord.File("runes1.png")
-        )
-        web_driver.quit()
-        await ctx.send("Reached end of cmd")
-@client.command()
-async def levels(ctx, champ, lane):
-    async with ctx.typing():
-        print('ran into cmd')
-        google_chrome_options = webdriver.ChromeOptions()
-        google_chrome_options.add_argument('--disable-gpu')
-        google_chrome_options.add_argument('--no-sandbox')
-        google_chrome_options.add_argument('--disable-dev-shm-usage')
-        google_chrome_options.add_argument('--enable-features=NetworkService,NetworkServiceInProcess')
-        google_chrome_options.binary_location= os.environ.get('GOOGLE_CHROME_BIN')
-        google_chrome_options.headless = True
-        google_chrome_options.add_argument('--window-size=1920,3000')
-        web_driver = webdriver.Chrome(
-            executable_path=os.environ.get('CHROMEDRIVER_PATH'),
-            options=google_chrome_options
-        )
-        url = f"https://op.gg/champion/{champ}/statistics/{lane}"
-        await ctx.send("Levels :\n")
-    async with ctx.typing():
-        web_driver.get(url)
-        web_driver.save_screenshot("element.png")
-        im = Image.open('element.png')
-        im = im.crop((420, 1000, 1150, 1170))
-        web_driver.close()
-        im.save('skills1.png')
-        await ctx.send(
-            file=discord.File("skills1.png")
-        )
-        web_driver.quit()
-        await ctx.send("Reached end of cmd")
-@client.command()
-async def items(ctx, champ, lane):
-    async with ctx.typing():
-        print('ran into cmd')
-        google_chrome_options = webdriver.ChromeOptions()
-        google_chrome_options.add_argument('--disable-gpu')
-        google_chrome_options.add_argument('--no-sandbox')
-        google_chrome_options.add_argument('--disable-dev-shm-usage')
-        google_chrome_options.headless = True
-        google_chrome_options.add_argument('--window-size=1920,3000')
-        google_chrome_options.add_argument('--enable-features=NetworkService,NetworkServiceInProcess')
-        google_chrome_options.binary_location= os.environ.get('GOOGLE_CHROME_BIN')
-        web_driver = webdriver.Chrome(
-            executable_path=os.environ.get('CHROMEDRIVER_PATH'),
-            options=google_chrome_options
-        )
-        url = f"https://op.gg/champion/{champ}/statistics/{lane}"
-        await ctx.send("Items :\n")
-    async with ctx.typing():
-        web_driver.get(url)
-        web_driver.save_screenshot("element.png")
-        im = Image.open('element.png')
-        im = im.crop((420, 1185, 1150, 1850))
-        im.save('items1.png')
-        await ctx.send(
-            file=discord.File("items1.png")
-        )
-        web_driver.quit()
-        await ctx.send("Reached end of cmd")
-@client.command()
-async def test(ctx):
-    await ctx.send("Bot oc cho")
-my_secret = os.environ.get("TOKEN")
-client.run(my_secret)
+async def b(ctx,champ,lane,index):
+    perk = [
+        ('Runes 1:',[]),
+        ('Runes 2:',[]),
+        ('Runes 3:',[]),
+        ('Runes 4:',[])
+    ]
+    op_url = f'https://vn.op.gg/champion/{champ}/statistics/{lane}/build'
+    data = requests.get(op_url)
+    soup = BeautifulSoup(data.text,'html.parser')
+    #r1
+    r1 = soup.find_all("tbody", {"class": "tabItem ChampionKeystoneRune-1"})
+    wrap1 = r1[0].find_all("div", {"class": "perk-page-wrap"})
+    bigrunes0 = wrap1[0].find_all("div",{"class": "perk-page__item perk-page__item--keystone perk-page__item--active"})
+    bigrunes1 = wrap1[1].find_all("div",{"class": "perk-page__item perk-page__item--keystone perk-page__item--active"})
+    bigrunes0_img =bigrunes0[0].find_all("img")
+    bigrunes1_img =bigrunes1[0].find_all("img")
+    perk[0][1].append('https:' + bigrunes0_img[0]['src'])
+    perk[1][1].append('https:' + bigrunes1_img[0]['src'])
 
+    smallrunes0 = wrap1[0].find_all("div",{"class": "perk-page__item perk-page__item--active"})
+    smallrunes1 = wrap1[1].find_all("div",{"class": "perk-page__item perk-page__item--active"})
+    print(len(smallrunes0))
+    for i in range(0,5):
+        smallrunes0_img = smallrunes0[i].find_all("img")
+        smallrunes1_img = smallrunes1[i].find_all("img")
+        perk[0][1].append('https:' + smallrunes0_img[0]['src'])
+        perk[1][1].append('https:' + smallrunes1_img[0]['src'])
+    #####################r2
+    r2 = soup.find_all("tbody", {"class": "tabItem ChampionKeystoneRune-2"})
+    wrap2 = r2[0].find_all("div", {"class": "perk-page-wrap"})
+    bigrunes2 = wrap2[0].find_all("div", {"class": "perk-page__item perk-page__item--keystone perk-page__item--active"})
+    bigrunes3 = wrap2[1].find_all("div", {"class": "perk-page__item perk-page__item--keystone perk-page__item--active"})
+    bigrunes2_img = bigrunes2[0].find_all("img")
+    bigrunes3_img = bigrunes3[0].find_all("img")
+    perk[2][1].append('https:' + bigrunes2_img[0]['src'])
+    perk[3][1].append('https:' + bigrunes3_img[0]['src'])
+
+    smallrunes2 = wrap2[0].find_all("div", {"class": "perk-page__item perk-page__item--active"})
+    smallrunes3 = wrap2[1].find_all("div", {"class": "perk-page__item perk-page__item--active"})
+    print(len(smallrunes2))
+    for i in range(0, 5):
+        smallrunes2_img = smallrunes2[i].find_all("img")
+        smallrunes3_img = smallrunes3[i].find_all("img")
+        perk[2][1].append('https:' + smallrunes2_img[0]['src'])
+        perk[3][1].append('https:' + smallrunes3_img[0]['src'])
+    print(len(perk[int(index)][1]))
+    for i in range(0, 6):
+        await ctx.send(perk[int(index)][1][i])
+@client.command()
+async def c(ctx,champ,lane):
+    perk = [
+        ('Runes 1:',[]),
+        ('Runes 2:',[]),
+        ('Runes 3:',[]),
+        ('Runes 4:',[])
+    ]
+    op_url = f'https://vn.op.gg/champion/{champ}/statistics/{lane}/build'
+    data = requests.get(op_url)
+    soup = BeautifulSoup(data.text,'html.parser')
+    #r1
+    r1 = soup.find_all("tbody", {"class": "tabItem ChampionKeystoneRune-1"})
+    wrap1 = r1[0].find_all("div", {"class": "perk-page-wrap"})
+    bigrunes0 = wrap1[0].find_all("div",{"class": "perk-page__item perk-page__item--keystone perk-page__item--active"})
+    bigrunes1 = wrap1[1].find_all("div",{"class": "perk-page__item perk-page__item--keystone perk-page__item--active"})
+    bigrunes0_img =bigrunes0[0].find_all("img")
+    bigrunes1_img =bigrunes1[0].find_all("img")
+    perk[0][1].append(bigrunes0_img[0]['alt'])
+    perk[1][1].append(bigrunes1_img[0]['alt'])
+
+    smallrunes0 = wrap1[0].find_all("div",{"class": "perk-page__item perk-page__item--active"})
+    smallrunes1 = wrap1[1].find_all("div",{"class": "perk-page__item perk-page__item--active"})
+    print(len(smallrunes0))
+    for i in range(0,5):
+        smallrunes0_img = smallrunes0[i].find_all("img")
+        smallrunes1_img = smallrunes1[i].find_all("img")
+        perk[0][1].append(smallrunes0_img[0]['alt'])
+        perk[1][1].append(smallrunes1_img[0]['alt'])
+    #####################r2
+    r2 = soup.find_all("tbody", {"class": "tabItem ChampionKeystoneRune-2"})
+    wrap2 = r2[0].find_all("div", {"class": "perk-page-wrap"})
+    bigrunes2 = wrap2[0].find_all("div", {"class": "perk-page__item perk-page__item--keystone perk-page__item--active"})
+    bigrunes3 = wrap2[1].find_all("div", {"class": "perk-page__item perk-page__item--keystone perk-page__item--active"})
+    bigrunes2_img = bigrunes2[0].find_all("img")
+    bigrunes3_img = bigrunes3[0].find_all("img")
+    perk[2][1].append(bigrunes2_img[0]['alt'])
+    perk[3][1].append(bigrunes3_img[0]['alt'])
+
+    smallrunes2 = wrap2[0].find_all("div", {"class": "perk-page__item perk-page__item--active"})
+    smallrunes3 = wrap2[1].find_all("div", {"class": "perk-page__item perk-page__item--active"})
+    print(len(smallrunes2))
+    for i in range(0, 5):
+        smallrunes2_img = smallrunes2[i].find_all("img")
+        smallrunes3_img = smallrunes3[i].find_all("img")
+        perk[2][1].append(smallrunes2_img[0]['alt'])
+        perk[3][1].append(smallrunes3_img[0]['alt'])
+    hero = soup.find_all("div",{"class": "champion-stats-header-info__image"})
+    hero_img = hero[0].find_all("img")
+    hero_img_url = 'https:' + hero_img[0]['src']
+    emb = discord.Embed(title=f'{str(champ).upper()} RUNES ON VNOPGG',color=discord.Colour.green())
+    emb.set_thumbnail(url=hero_img_url)
+    emb.add_field(name='Build number 0:', value= perk[0][1],inline= True)
+    emb.add_field(name='Build number 1:', value= perk[1][1],inline= False)
+    emb.add_field(name='Build number 2:', value= perk[2][1],inline= False)
+    emb.add_field(name='Build number 3:', value= perk[3][1],inline= True)
+    emb.set_author(name=f'{str(champ).capitalize()}',icon_url=hero_img_url)
+    await ctx.send(embed=emb)
+
+
+keep_alive()
+client.run(os.environ.get('TOKEN'))
